0a0cd7bd3025ac98387acacf15da3fea33854dc1
Adrian Biedrzycki <a@modules.io>
2017-12-16 10:18:30 -0800

    Major caching optimizations

--- index.js
+++ index.js
@@ -1,71 +1,106 @@
 const http = require('http');
 
 const express = require('express');
+const etag = require('etag');
 const puppeteer = require('puppeteer');
 
 const port = process.env['PORT'] || 8080;
 const size = 1280;
+const maxAge = 10 * 60 * 1000;
+
+const LRU = require('lru-cache');
+const cache = new LRU({
+  max: 100,
+  maxAge,
+});
 
 const app = express();
 app.use('/static', express.static(__dirname));
 app.get('/render', (req, res, next) => {
   const {u} = req.query;
 
-  (async () => {
-    try {
-      const page = await browser.newPage();
-      await page.setViewport({
-        width: size,
-        height: size,
-      });
-      page.on('error', err => {
-        res.status(500);
-        res.end(err.stack);
-      });
-      page.on('pageerror', err => {
-        res.status(500);
-        res.end(err.stack);
-      });
-      /* page.on('request', req => {
-        console.log('request', req.url);
-      });
-      page.on('response', res => {
-        console.log('response', res.url);
-      });
-      page.on('requestfinished', req => {
-        console.log('requestfinished', req.url);
-      });
-      page.on('requestfailed', err => {
-        console.log('request failed', err);
-      });  */
-      page.on('console', async msg => {
-        if (msg.type === 'log' && msg.args.length === 1 && msg.args[0]._remoteObject.value === 'loaded') {
-          const buffer = await page.screenshot();
-          res.type('image/png');
-          res.end(buffer);
+  const entry = cache.get(u);
+  if (entry) {
+    res.type('image/jpeg');
+    res.set('Etag', entry.etag);
+    res.set('Cache-Control', `public, max-age=${maxAge}`);
 
-          page.close();
-          clearTimeout(timeout);
-        } else if (msg.type === 'warning' && msg.args.length === 3 && msg.args[0]._remoteObject.value === 'error') {
-          res.status(msg.args[1]._remoteObject.value);
-          res.end(msg.args[2]._remoteObject.value);
-
-          page.close();
-          clearTimeout(timeout);
-        }
-      });
-      const timeout = setTimeout(() => {
-        res.status(500);
-        res.end('timed out');
-
-        page.close();
-      }, 5 * 1000);
-      await page.goto(`http://127.0.0.1:${port}/static?u=${encodeURIComponent(u)}&s=${size}`);
-    } catch(err) {
-      res.status(500);
-      res.end(err.stack);
+    if (req.headers['if-none-match'] === entry.etag) {
+      res.status(304);
+      res.end();
+    } else {
+      res.end(entry);
     }
-  })();
+  } else {
+    (async () => {
+      try {
+        const page = await browser.newPage();
+        await page.setViewport({
+          width: size,
+          height: size,
+        });
+        page.on('error', err => {
+          res.status(500);
+          res.end(err.stack);
+        });
+        page.on('pageerror', err => {
+          res.status(500);
+          res.end(err.stack);
+        });
+        /* page.on('request', req => {
+          console.log('request', req.url);
+        });
+        page.on('response', res => {
+          console.log('response', res.url);
+        });
+        page.on('requestfinished', req => {
+          console.log('requestfinished', req.url);
+        });
+        page.on('requestfailed', err => {
+          console.log('request failed', err);
+        });  */
+        page.on('console', async msg => {
+          if (msg.type === 'log' && msg.args.length === 1 && msg.args[0]._remoteObject.value === 'loaded') {
+            const buffer = await page.screenshot({
+              type: 'jpeg',
+            });
+            buffer.etag = etag(buffer);
+
+            cache.set(u, buffer);
+
+            res.type('image/jpeg');
+            res.set('Etag', buffer.etag);
+            res.set('Cache-Control', `public, max-age=${maxAge}`);
+            if (req.headers['if-none-match'] === buffer.etag) {
+              res.status(304);
+              res.end();
+            } else {
+              res.end(buffer);
+            }
+
+            page.close();
+            clearTimeout(timeout);
+          } else if (msg.type === 'warning' && msg.args.length === 3 && msg.args[0]._remoteObject.value === 'error') {
+            res.status(msg.args[1]._remoteObject.value);
+            res.end(msg.args[2]._remoteObject.value);
+
+            page.close();
+            clearTimeout(timeout);
+          }
+        });
+        const timeout = setTimeout(() => {
+          res.status(500);
+          res.end('timed out');
+
+          page.close();
+        }, 5 * 1000);
+        await page.goto(`http://127.0.0.1:${port}/static?u=${encodeURIComponent(u)}&s=${size}`);
+      } catch(err) {
+        res.status(500);
+        res.end(err.stack);
+      }
+    })();
+  }
 });
 
 let browser = null;
